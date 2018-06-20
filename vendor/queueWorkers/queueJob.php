e54d7b561786b0adc579902344e39f7588c59435
naknak987 <naknak987@gmail.com>
2018-06-20 17:57:57 -0400

    The time to run needs to be more than it takes to finish the job.

--- vendor/queueWorkers/queueJob.php
+++ vendor/queueWorkers/queueJob.php
@@ -22,12 +22,20 @@ class queueJob
          * This function will search for new jobs to add to the queue.
          * It should only run in the parent loop above. 
          */
+            /**
+             * Puts a job on the queue.
+             *
+             * @param string $data     The job data
+             * @param int    $priority From 0 (most urgent) to 0xFFFFFFFF (least urgent)
+             * @param int    $delay    Seconds to wait before job becomes ready
+             * @param int    $ttr      Time To Run: seconds a job can be reserved for
+             */
 
         sleep(2);
         echo ".";
         $this->q
             ->useTube('testtube')
-            ->put("3", 10, 0, 2);
+            ->put("3", 10, 0, 5);
     }
 }
 
